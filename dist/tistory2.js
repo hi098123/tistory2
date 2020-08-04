7b94f05d2915a5c7d9dc3621b4b34866401b50f0
hi098123 <46213700+hi098123@users.noreply.github.com>
2020-08-05 01:44:35 +0900

    Update tistory2.js

--- dist/tistory2.js
+++ dist/tistory2.js
@@ -5,7 +5,7 @@
 			var parent=comm.parentNode;
 			var notice=document.createElement('div');
 			notice.id='hi098123-tistory2';
-			notice.innerHTML='<span>티스토리 로그인이 해제되었나요?</span><span><a href="https://www.tistory.com/auth/login/?redirectUrl='+encodeURIComponent(TistoryBlog.tistoryUrl+window.location.pathname+'#'+parent.id)+'" rel="noopener noreferrer">로그인</a>하여 댓글 남기러가기</span><style>#hi098123-tistory2{text-align:center;border:2px solid #ff9800;background:#fff3e0;padding:10px 0;color: #333;}#hi098123-tistory2>span{margin: auto 3px;display:inline-block}</style>';
+			notice.innerHTML='<span>티스토리 로그인이 해제되었나요?</span><span><a href="https://www.tistory.com/auth/login/?redirectUrl='+encodeURIComponent(TistoryBlog.tistoryUrl+window.location.pathname+'#'+parent.id)+'" rel="noopener noreferrer">로그인</a>하여 댓글 남기러가기</span><a href="https://hi098123-skin.tistory.com/22" target="_blank" rel="noopener" style="color: #fff;position: absolute;right: 0;background-color: #aaa;width: 15px;font-size: 12px;top: 0;margin: 0;border-radius: 5px;" title="적용방법 : 제작 hi098123">i</a><style>#hi098123-tistory2{text-align:center;border:2px solid #ff9800;background:#fff3e0;padding:10px 0;color: #333;position: relative}#hi098123-tistory2>span{margin: auto 3px;display:inline-block}</style>';
 			parent.insertBefore(notice,comm);
     	}
     }
