c25fe570af3a5406a01447f90c38b9106d0550e6
hi098123 <46213700+hi098123@users.noreply.github.com>
2021-05-27 23:10:13 +0900

    Update tistory2.js

--- src/tistory2.js
+++ src/tistory2.js
@@ -4,8 +4,10 @@
     		var comm=document.querySelector('form[action*="/comment/add/"]');
 			var parent=comm.parentNode;
 			var notice=document.createElement('div');
+			var num='';
+			if(window.location.pathname.indexOf('/guestbook')>-1){num=0}else if(typeof(ReactionReqBody)=='object'){num=ReactionReqBody.entryId}
 			notice.id='hi098123-tistory2';
-			notice.innerHTML='<span>티스토리 로그인이 해제되었나요?</span><span><a href="https://www.tistory.com/auth/login/?redirectUrl='+encodeURIComponent(TistoryBlog.tistoryUrl+window.location.pathname+'#entry'+ReactionReqBody.entryId+'Comment')+'" rel="noopener noreferrer">로그인</a>하여 댓글 남기러가기</span><a href="https://hi098123-skin.tistory.com/22" target="_blank" rel="noopener" style="color: #fff;position: absolute;right: 0;background-color: #d2d2d2;width: 15px;font-size: 12px;top: 0;margin: 0;border-radius: 5px;line-height: 15px;z-index: 99999;" title="적용방법 : 제작 hi098123">i</a><style>#hi098123-tistory2{text-align:center;border:2px solid #ff9800;background:#fff3e0;padding:10px 0;color: #333;position: relative}#hi098123-tistory2>span{margin: auto 3px;display:inline-block}</style>';
+			notice.innerHTML='<span>티스토리 로그인이 해제되었나요?</span><span><a href="https://www.tistory.com/auth/login/?redirectUrl='+encodeURIComponent(TistoryBlog.tistoryUrl+window.location.pathname+'#entry'+num+'Comment')+'" rel="noopener noreferrer">로그인</a>하여 댓글 남기러가기</span><a href="https://hi098123-skin.tistory.com/22" target="_blank" rel="noopener" style="color: #fff;position: absolute;right: 0;background-color: #d2d2d2;width: 15px;font-size: 12px;top: 0;margin: 0;border-radius: 5px;line-height: 15px;z-index: 99999;" title="적용방법 : 제작 hi098123">i</a><style>#hi098123-tistory2{text-align:center;border:2px solid #ff9800;background:#fff3e0;padding:10px 0;color: #333;position: relative}#hi098123-tistory2>span{margin: auto 3px;display:inline-block}</style>';
 			parent.insertBefore(notice,comm);
     	}
     }
